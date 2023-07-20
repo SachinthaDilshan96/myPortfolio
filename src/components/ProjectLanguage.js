6a4de06615bc35528d23c19dae758c1be6516eea
sachinthadilshan <sachinthadilshan96@gmail.com>
2023-07-20 05:59:01 +0530

    favicon changed

--- src/components/ProjectLanguage.js
+++ src/components/ProjectLanguage.js
@@ -4,7 +4,7 @@ export const ProjectLanguage=({languages,isAll})=>{
     function getRandom(){
         return Math.floor(Math.random() * 11);
     }
-    const colors = ["bg-lime-600","bg-red-600","bg-orange-600","bg-amber-600","bg-neutral-200","bg-rose-600","bg-fuchsia-600","bg-indigo-500","bg-cyan-700","bg-emerald-300"];
+    const colors = ["bg-lime-600","bg-red-600","bg-orange-600","bg-amber-600","bg-neutral-600","bg-rose-600","bg-fuchsia-600","bg-indigo-500","bg-cyan-700","bg-emerald-600"];
     if (isAll) {
         return(
             <div className={"flex justify-center"}>
